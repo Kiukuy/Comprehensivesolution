a6f07b27b3472db52c125aa47345367fba96990d
Kiukuy <1489834520@qq.com>
2024-12-20 22:41:24 +0800

    feat(element-plus): 初始化结构，新增elementplus功能

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  //继承的规则
+  // 继承的规则
   extends: ['@commitlint/config-conventional'],
-//定义规则类型
-rules: {
-  //type类型定义，表示git提交的type必须在以下类型范围内
-  'type-enum': [
-    2,
-    'always',
-    [
-      'feat', //新功能 feature
-      'fix', //修复bug
-      'docs', //文档注释
-      'style', //代码格式（不影响代码允许的变动）
-      'refactor', //重构（既不增加新功能，也不是修复bug）
-      'perf', //性能优化
-      'test', //增加测试
-      'chore', //构建过程或辅助工具的变动
-      'revert', //回退
-      'build' //打包
-    ]
-  ],
-  // subject大小写不做校验
-  'subject-case': [0]
+  // 定义规则类型
+  rules: {
+  // type类型定义，表示git提交的type必须在以下类型范围内
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // 新功能 feature
+        'fix', // 修复bug
+        'docs', // 文档注释
+        'style', // 代码格式（不影响代码允许的变动）
+        'refactor', // 重构（既不增加新功能，也不是修复bug）
+        'perf', // 性能优化
+        'test', // 增加测试
+        'chore', // 构建过程或辅助工具的变动
+        'revert', // 回退
+        'build' // 打包
+      ]
+    ],
+    // subject大小写不做校验
+    'subject-case': [0]
   }
 }
