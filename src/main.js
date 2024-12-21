cfdf089a87b6b79d1ad46685cbaeb9feec3c70fa
Kiukuy <1489834520@qq.com>
2024-12-21 21:50:05 +0800

    feat(login): 增加登录界面结构和样式

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
+import './style/index.scss'
+import './style/update-element.scss'
 
 const app = createApp(App)
 installElementPlus(app)
