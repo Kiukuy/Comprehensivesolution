ee8239e95e3590281f2d41df73135f1f0a6ddda9
Kiukuy <1489834520@qq.com>
2024-12-30 17:40:04 +0800

    fix(sidebar): 左侧边栏顶部logo

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,12 +1,38 @@
 <script setup>
 import SidebarMenu from './SidebarMenu.vue'
+const logoHeight = 44
 </script>
 
 <template>
   <div>
-    <h1>占位</h1>
+    <div class="logo-container">
+      <el-avatar
+        :size="logoHeight"
+        shape="square"
+        src="https://m.imooc.com/static/wap/static/common/img/logo-small@2x.png"
+      ></el-avatar>
+      <h1 class="logo-title" v-if="$store.getters.sidebarOpened">
+        imooc-admin
+      </h1>
+    </div>
     <SidebarMenu></SidebarMenu>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.logo-container {
+  height: v-bind(logoHeight) + 'px';
+  padding: 10px 0 22px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .logo-title {
+    margin-left: 10px;
+    color: #fff;
+    font-weight: 600;
+    line-height: 50px;
+    font-size: 16px;
+    white-space: nowrap;
+  }
+}
+</style>
