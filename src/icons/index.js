1adc4081eb278246d43870f428a46dfe559dc04c
Kiukuy <1489834520@qq.com>
2024-12-31 18:09:17 +0800

    feat(国际化): 初始化i18n

--- src/icons/index.js
+++ src/icons/index.js
@@ -3,7 +3,7 @@ import SvgIcon from '@/components/SvgIcon'
 // 通过 require.context()函数来创建自己的context
 const svgRequire = require.context('./svg', false, /\.svg$/)
 
-// 返回一个require函数，可以接受以恶搞request的参数，用于require的导入
+// 返回一个require函数，可以接受一个request的参数，用于require的导入
 
 svgRequire.keys().forEach(svgIcon => svgRequire(svgIcon))
 
--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import installElementPlus from './plugins/element'
-import './styles/index.scss'
-import './styles/update-element.scss'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
+import installElementPlus from '@/plugins/element'
+import '@/styles/index.scss'
+import '@/styles/update-element.scss'
 import installIcons from '@/icons'
-import './permission'
+import '@/permission'
+import i18n from '@/i18n'
 
 const app = createApp(App)
 installElementPlus(app)
 installIcons(app)
-app.use(store).use(router).mount('#app')
+app.use(store).use(router).use(i18n).mount('#app')
