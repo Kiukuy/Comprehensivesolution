7e544f97616cadffc0a75b888031ea06c36a65a3
Kiukuy <1489834520@qq.com>
2025-01-01 17:03:58 +0800

    feat(app.vue element.js): element 国际化处理

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <RouterView />
+  <el-config-provider :locale="store.getters.language === 'en' ? en : zhCn">
+    <RouterView />
+  </el-config-provider>
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
+<script setup>
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import en from 'element-plus/es/locale/lang/en'
+import { useStore } from 'vuex'
+
+const store = useStore()
 </script>
 
 <style></style>
