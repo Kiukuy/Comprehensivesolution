0490ebaab38538586311aded6b05f53266e4dc4d
Kiukuy <1489834520@qq.com>
2025-01-11 13:12:56 +0800

    feat(feature): 功能模块

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -7,15 +7,15 @@
       <el-col :span="18">
         <el-card>
           <el-tabs v-model="activeName">
-            <el-tab-pane :label="$t('msg.profile.feature')" name="feature"
-              ><feature
-            /></el-tab-pane>
-            <el-tab-pane :label="$t('msg.profile.chapter')" name="chapter"
-              ><chapter
-            /></el-tab-pane>
-            <el-tab-pane :label="$t('msg.profile.author')" name="author"
-              ><author
-            /></el-tab-pane>
+            <el-tab-pane :label="$t('msg.profile.feature')" name="feature">
+              <feature :features="featureData" />
+            </el-tab-pane>
+            <el-tab-pane :label="$t('msg.profile.chapter')" name="chapter">
+              <chapter />
+            </el-tab-pane>
+            <el-tab-pane :label="$t('msg.profile.author')" name="author">
+              <author />
+            </el-tab-pane>
           </el-tabs>
         </el-card>
       </el-col>
