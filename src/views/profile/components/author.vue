40f88d005ea3cb70ff86275b2f339f66b0970da7
Kiukuy <1489834520@qq.com>
2025-01-11 15:08:17 +0800

    feat(author): 作者模块

--- src/views/profile/components/author.vue
+++ src/views/profile/components/author.vue
@@ -1,9 +1,49 @@
 <script setup>
-import {} from 'vue'
+import PanThumb from '@/components/PanThumb'
 </script>
 
 <template>
-  <div>作者</div>
+  <div class="author-container">
+    <div class="header">
+      <PanThumb
+        image="https://res.lgdsunday.club/my-avatar.jpg"
+        height="60px"
+        width="60px"
+        :hoverable="false"
+      >
+        {{ $t('msg.profile.name') }}
+      </PanThumb>
+      <div class="header-desc">
+        <h3>{{ $t('msg.profile.name') }}</h3>
+        <span>{{ $t('msg.profile.job') }}</span>
+      </div>
+    </div>
+    <div class="info">
+      {{ $t('msg.profile.Introduction') }}
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.author-container {
+  .header {
+    display: flex;
+    .header-desc {
+      margin-left: 12px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+
+      span {
+        font-size: 14px;
+      }
+    }
+  }
+  .info {
+    margin-top: 16px;
+    line-height: 22px;
+    font-size: 14px;
+    text-indent: 26px;
+  }
+}
+</style>
